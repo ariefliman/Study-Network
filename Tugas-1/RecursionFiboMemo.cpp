edac25308516794ca9c2b4c7e369791e34fb6b63
Muhamad Arief Liman <54799155+ariefliman21@users.noreply.github.com>
2020-09-17 18:07:42 +0700

    RecursionFiboUpdate

--- Tugas-1/RecursionFiboMemo.cpp
+++ Tugas-1/RecursionFiboMemo.cpp
@@ -5,25 +5,18 @@ int memo[100];
 int fibo(int n){
 	
 	if(n == 0) return 0;
-	else if(n == 1 || n == 2) return 1;
+	else if(n == 1) return 1;
+	else if(memo[n] != -1) return memo[n];
+	
 	memo[n] = fibo(n-1)+fibo(n-2);
 	return memo[n];
 }
 
 
-int printFibo(int n){
-	return(memo[n] != -1) ? memo[n]:fibo(n);
-}
-
 int main(){
 	for(int i = 0; i  < 100; i++){
 		memo[i] = -1;
 	}
-	
-	memo[1] = 1;
-	memo[2] = 1;
-	
-	
-	printf("%d\n", printFibo(5));
-	printf("%d\n", printFibo(6));
+	printf("%d\n", fibo(5));
+	printf("%d\n", fibo(6));
 }
